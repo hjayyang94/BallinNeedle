6ea2582c8f278bd244946ac2f1d42b6227bb07c5
Jay Yang <hjyang1@uci.edu>
2018-03-17 14:00:01 -0700

    Fire needle script and circle rotation script

---
+++ Hit the Circle/Assets/Scripts/Circle/CircleRotateScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleRotateScript : MonoBehaviour {
+
+    [SerializeField]
+    private float rotationSpeed = 50f;
+
+    private bool canRotate;
+
+    private float angle;
+	// Use this for initialization
+	void Awake () {
+        canRotate = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (canRotate)
+        {
+            RotateCircle();
+        }
+	}
+
+    void RotateCircle()
+    {
+        angle = transform.rotation.eulerAngles.z;
+        angle += rotationSpeed * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+}
---
+++ Hit the Circle/Assets/Scripts/Needle/NeedleMovementScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeedleMovementScript : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject needleBody;
+
+    private bool canShootNeedle;
+    private bool touchedCircle;
+
+    private float speedY = 10f;
+
+    private Rigidbody2D myBody;
+
+	// Use this for initialization
+	void Awake () {
+        Initalize();
+        FireNeedle();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (canShootNeedle)
+        {
+            myBody.velocity = new Vector2(0, speedY);
+        }
+	}
+
+    void Initalize()
+    {
+        needleBody.SetActive(false);
+        myBody = GetComponent<Rigidbody2D>();
+    }
+
+    public void FireNeedle()
+    {
+        needleBody.SetActive(true);
+        myBody.isKinematic = false;
+        canShootNeedle = true;
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (touchedCircle)
+        {
+            return;
+            
+        }
+        
+        if (other.tag == "Circle")
+        {
+            canShootNeedle = false;
+            touchedCircle = true;
+            myBody.velocity = Vector2.zero;
+            myBody.isKinematic = true;
+            gameObject.transform.SetParent(other.transform);
+            
+            
+
+        }
+    }
+}
