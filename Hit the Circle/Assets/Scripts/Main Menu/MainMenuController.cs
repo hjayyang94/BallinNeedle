15a605a28c3566ed9369ba45d70c197c664609e0
Jay Yang <hjyang1@uci.edu>
2018-03-19 16:47:46 -0700

    Added main menu and play button working

---
+++ Hit the Circle/Assets/Scripts/Main Menu/MainMenuController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour {
+
+    Button playButton;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Practice");
+    }
+
+    void GetButton()
+    {
+        playButton = GameObject.Find("Shoot Button").GetComponent<Button>();
+    }
+}
